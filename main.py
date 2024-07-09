c1e9a76494408f88c9a0b426d734465f9994c63c
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-07-08 21:57:14 -0300

    Atualizado o valor da op na função 'get_data'

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from get_data import LabelData
 from label_print import LabelPrint
 
-exp = LabelData('./ordem.xlsx')
-exp.print_data()
-exp.get_data('OP-0216534')
-
-lb = LabelPrint(exp.get_data('OP-0215869'))
-lb.create_label()
-
 if __name__ == "__main__":
-  print("Hello World")
+  exp = LabelData('./ordem.xlsx')
+  exp.print_data()
+  lb = LabelPrint(exp.get_data('OP-0216534'))
+  lb.create_label()
