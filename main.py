2106a3402400e0c56478e737f470b12f30b78d18
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-07-10 20:46:58 -0300

    Alterado o numero da op, feito teste com a funcção print_label()

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from label_print import LabelPrint
 
 if __name__ == "__main__":
   exp = LabelData('./ordem.xlsx')
-  exp.print_data()
   lb = LabelPrint(exp.get_data('OP-0216534'))
   lb.create_label()
+  lb.print_label()
