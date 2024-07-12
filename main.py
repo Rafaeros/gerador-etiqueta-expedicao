31b59d9bc1161bd4ab63a3a949f5d9693b0b35ed
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-07-11 22:19:03 -0300

    Alterado a estrutura para chamar a interface

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from get_data import LabelData
 from label_print import LabelPrint
+from interface import Interface
 
 if __name__ == "__main__":
-  exp = LabelData('./ordem.xlsx')
-  lb = LabelPrint(exp.get_data('OP-0216534'))
-  lb.create_label()
-  lb.print_label()
+  app = Interface()
+  app.run()
