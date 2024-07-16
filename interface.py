d8f965979046580d8271ee0f863816bc596f7cd5
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-07-16 00:38:33 -0300

    Adicionado função para limpar os inputs após impressão, campo para inserir o peso, e tratamento de erros

--- interface.py
+++ interface.py
@@ -7,8 +7,9 @@ import time
 class Interface:
   def __init__(self):
     self.master = ctk.CTk()
-    self.master.geometry("650x500")
+    self.master.geometry("800x600")
     self.master.resizable(False, False)
+    self.master.title("Gerador De Etiquetas")
     self.create_variables()
     self.create_window()
 
@@ -39,6 +40,9 @@ class Interface:
     self.quantity_label = ctk.CTkLabel(self.master, text="Quantidade Total:")
     self.quantity_input = ctk.CTkEntry(self.master, placeholder_text="Quantidade Total", width=200, height=35)
 
+    self.weight_label = ctk.CTkLabel(self.master, text="Peso: ")
+    self.weight_input = ctk.CTkEntry(self.master, placeholder_text="Peso: 0,00 Kg")
+
     self.print_button = ctk.CTkButton(self.master, text="Imprimir", command=self.print_label, width=150, height=50, corner_radius=10)
 
     padding = {'padx': 5, 'pady': 10}
@@ -63,28 +67,39 @@ class Interface:
     self.quantity_label.grid(row=7, column=1, **padding)
     self.quantity_input.grid(row=7, column=2, **padding)
 
-    self.print_button.grid(row=10, column=2, **padding)
+    self.weight_label.grid(row=7, column=3)
+    self.weight_input.grid(row=7, column=4)
+
+    self.print_button.grid(row=10, column=2, columnspan=3, pady=20)
 
   def create_variables(self):
-    self.label_data = LabelData('./ordem.xlsx')
-    self.label_data_df = self.label_data.label_data
+    try:
+      self.label_data = LabelData()
+      self.label_data_df = self.label_data.label_data
+    except Exception as e:
+      msg = ctkmsg(self.master, title=f"Erro ao carregar planilha", message=f"Não encontrado o arquivo: ordens.xlsx com a data de hoje ({e})", icon="warning", option_1="OK")
+      msg.wait_window()
+      if msg.get() == "OK":
+        self.master.destroy()
+      
     self.client_var = ctk.StringVar()
     self.code_var = ctk.StringVar()
     self.description_var = ctk.StringVar()
     self.barcode_var = ctk.StringVar()
     self.quantity_var = ctk.StringVar()
+    self.weight_var = ctk.StringVar()
     self.id = ''
 
   def search_id(self, event=None):
     self.id = self.id_input.get()
     if (self.label_data_df['Código'] == self.id).any():
       try:
-        info = self.label_data.get_data(self.id)
+        info = self.label_data.get_data(self.id, "")
         self.client_var.set(info.client)
         self.code_var.set(info.code)
         self.description_var.set(info.description)
         self.barcode_var.set(info.barcode)
-        self.quantity_var.set(info.quantity) 
+        self.quantity_var.set(info.quantity)
 
         if(self.client_input.get()==""):
           self.client_input.insert(0, self.client_var.get())
@@ -107,9 +122,15 @@ class Interface:
     self.description_input.delete(0, ctk.END)
     self.barcode_input.delete(0, ctk.END)
     self.quantity_input.delete(0, ctk.END)
+    self.weight_input.delete(0, ctk.END)
 
   def print_label(self):
-    label = LabelPrint(self.label_data.get_data(self.id))
-    label.create_label()
-    time.sleep(1)
-    label.print_label()
+    try:
+      label = LabelPrint(self.label_data.get_data(self.id, self.weight_input.get()))
+      label.create_label()
+      time.sleep(1)
+      label.print_label()
+    except Exception as e:
+      ctkmsg(self.master, message=f"Erro ao imprimir: {e}", title="Erro", icon="cancel", option_1="OK")
+    finally:
+      self.clear_inputs()
