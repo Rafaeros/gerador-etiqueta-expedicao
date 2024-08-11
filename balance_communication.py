b4750c25051db263e468c0b40c9364c6af7616d2
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-08-11 20:32:25 -0300

    Finalizado a configuração da balança e formatação da resposta da balança

--- balance_communication.py
+++ balance_communication.py
@@ -3,31 +3,45 @@ from time import sleep
 
 class Serial():
     def __init__(self):
-        self.port: str
-        self.baud_rate: int = 9600
-        self.timeout: int = 2 # Tempo de espera para leitura
+        self.set_baud_rate()
+        self.set_timeout()
 
     def set_port(self, port):
-        self.port = port
+        self.port: str = port
 
-    def connect(self):
+    def set_baud_rate(self, rate=9600):
+        self.baud_rate: int = rate
+
+    def set_timeout(self, timeout=1):
+        self.timeout: int = timeout
+
+    def connect(self) -> str:
         try:
             # Abrindo a conexão serial
             self.serial = serial.Serial(self.port, self.baud_rate, timeout=self.timeout)
-            print(f'Conectado à porta {self.port} com baud rate {self.baud_rate}')
+            sleep(0.5)
+            return f"Conectado a porta {self.port} com sucesso:"
 
         except serial.SerialException as e:
-            print(f'Erro na comunicação: {e}')
-            return e
+            return f"Erro ao conectar com a porta {self.port}: {e}"
 
-    def read_serial(self):
+    def read_serial(self) -> str:
         while True:
                 # Obtendo resposta da balança e formatando o o peso
                 response = self.serial.readline().decode().strip()
                 if(response and response[0]=="D"):
-                    weight = response[1:].split('#')[0]
-                    weight = float(weight)
-                    formatted_weight = f"{weight:.2f}"
-                    return formatted_weight
+                    try:
+                        weight = response[1:].split('#')[0]
+                        weight = float(weight)
+                        formatted_weight = f"{weight:.2f}".lstrip('0').rstrip('.')
+
+                        if formatted_weight == '':
+                            formatted_weight = '0.00'
+
+                        formatted_weight = formatted_weight.replace('.', ',')
+
+                        return formatted_weight
+                    except ValueError:
+                        print(f'Erro ao converter "{response}" para float.')
                 # Leitura a cada 0.5 segundos
                 sleep(0.5)
