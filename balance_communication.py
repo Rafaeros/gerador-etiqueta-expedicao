081060873179def7c2f8219b7671dfeb6cfbf018
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2024-08-11 15:58:16 -0300

    Criado a formatação da resposta da balança

--- balance_communication.py
+++ balance_communication.py
@@ -1,7 +1,7 @@
 import serial
 from time import sleep
 
-class serial_com():
+class Serial():
     def __init__(self):
         self.port: str
         self.baud_rate: int = 9600
@@ -10,23 +10,24 @@ class serial_com():
     def set_port(self, port):
         self.port = port
 
-    def connection(self):
+    def connect(self):
         try:
             # Abrindo a conexão serial
             self.serial = serial.Serial(self.port, self.baud_rate, timeout=self.timeout)
             print(f'Conectado à porta {self.port} com baud rate {self.baud_rate}')
+
         except serial.SerialException as e:
             print(f'Erro na comunicação: {e}')
+            return e
 
-    def read_serial(self) -> str:
-        sleep(5)
-        # Lendo a resposta da balança
-        response = self.serial.readline().decode().strip()
-        print(f'Resposta da balança: {response}')
-        self.serial.close()
-        return response
-
-con = serial_com()
-con.set_port('COM5')
-con.connection()
-con.read_serial()
+    def read_serial(self):
+        while True:
+                # Obtendo resposta da balança e formatando o o peso
+                response = self.serial.readline().decode().strip()
+                if(response and response[0]=="D"):
+                    weight = response[1:].split('#')[0]
+                    weight = float(weight)
+                    formatted_weight = f"{weight:.2f}"
+                    return formatted_weight
+                # Leitura a cada 0.5 segundos
+                sleep(0.5)
