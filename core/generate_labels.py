e232b976e206ff89b725049b81652f1a9932184a
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2025-02-14 20:08:31 -0300

    [feat] pdf label generator

---
+++ core/generate_labels.py
@@ -0,0 +1,70 @@
+import pathlib
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.graphics.barcode.code128 import Code128
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from reportlab.lib.styles import getSampleStyleSheet
+from get_data import OrdemDeProducao
+
+LABEL_SIZE: int = (428, 283)
+MARGIN: int = 14
+FONTS_PATH: pathlib.Path = pathlib.Path(__file__).parent / "assets/fonts"
+LABEL_FONTS: list = [TTFont("FiraCodeRegular", FONTS_PATH / "FiraCode-Regular.ttf"), TTFont("FiraCodeBold", FONTS_PATH / "FiraCode-Bold.ttf")]
+
+class Label(Canvas):
+    op: OrdemDeProducao
+    def __init__(self, op: OrdemDeProducao) -> None:
+        super().__init__("label.pdf", LABEL_SIZE)
+        self.op = op
+        for font in LABEL_FONTS:
+            pdfmetrics.registerFont(font)
+
+    def _draw_text(self, x: int, y: int, text: str, font = "FiraCodeRegular", font_size = 10):
+        styles = getSampleStyleSheet()
+        style = styles["Normal"]
+        text.replace("\n", "<br/>")
+        style.fontName = font
+        style.fontSize = font_size
+        paragraph = Paragraph(text, style)
+        paragraph.wrapOn(self, 460, 283)
+        paragraph.drawOn(self, x, y)
+
+    def _create_label_barcode(self, x: int, y: int, barcode: str, barWidth = 1.4, barHeight = 30, humanReadable = True, max_width = LABEL_SIZE[0], font = "FiraCodeRegular", font_size = 10, min_scale = 1.1):
+        barcode = Code128(barcode, barWidth = barWidth, barHeight = barHeight, humanReadable = True, fontName = font, fontSize = font_size)
+        scale = min((max_width-x) / barcode.width, min_scale)
+        self.saveState()
+        self.scale(scale, 1)
+        barcode.drawOn(self, x/scale, y)
+        self.restoreState()
+
+    def generate_label(self):
+        elements_positions = {
+            "date": {"x": MARGIN, "y": 215, "text": self.op.today, "font": "FiraCodeRegular", "font_size": 12},
+            "line": {"x1": 0, "y1": 200, "x2": 428, "y2": 200},
+            "client": {"x": MARGIN, "y": 170, "text": f"CLIENTE: {self.op.client}", "font": "FiraCodeRegular", "font_size": 12},
+            "material_code": {"x": MARGIN, "y": 150, "text": f"CODIGO: {self.op.material_code}", "font": "FiraCodeBold", "font_size": 18},
+            "description": {"x": MARGIN, "y": 115, "text": f"DESCRICAO: {self.op.description}", "font": "FiraCodeRegular", "font_size": 12},
+            "barcode": {"x": -5, "y": 65, "text": self.op.barcode, "font": "FiraCodeRegular", "font_size": 12},
+            "quantity": {"x": MARGIN, "y": 20, "text": f"QUANTIDADE: {self.op.quantity}", "font": "FiraCodeRegular", "font_size": 12},
+            "quantity_barcode" : {"x": 150, "y": 15, "text": self.op.quantity, "font": "FiraCodeRegular", "font_size": 12},
+            "weight": {"x": 330, "y": 20, "text": f"{self.op.weight} KG", "font": "FiraCodeRegular", "font_size": 12}
+        }
+
+        for key, value in elements_positions.items():
+            if key == "line":
+                self.line(value["x1"], value["y1"], value["x2"], value["y2"])
+                continue
+            if "barcode" in key:
+                self._create_label_barcode(value["x"], value["y"], value["text"], font = value["font"], font_size = value["font_size"])
+                continue
+            if "quantity_barcode" in key:
+                self._create_label_barcode(value["x"], value["y"], value["text"], font = value["font"], font_size = value["font_size"], max_width = 100, min_scale = 1)
+                continue
+            self._draw_text(value["x"], value["y"], value["text"], value["font"], value["font_size"])
+        self.save()
+
+if __name__ == "__main__":
+    op = OrdemDeProducao(223536, "CEA085 000 000", "CEABS SERVIÇOS ELETRONICOS LTDA", "MODULO ISCA 2GKF TESTADOR CONTROLE MWM <br/>RAPIDO 20040 OEOEOE030 adfadwadawdawdawdaw", "CEA085 000 000 (225664)", 2000, 2, 10.50)
+    lb = Label(op)
+    lb.generate_label()
